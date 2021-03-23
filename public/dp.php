88fe691c580170dbb0193ec082c0516f533cc38d
MalesAnte <ante.males67@gmail.com>
2021-03-23 21:39:56 +0100

    DODANO

--- public/dp.php
+++ public/dp.php
@@ -1,7 +1,7 @@
 <?php 
 
-$konekcija=mysqli_connect('localhost','root','','stranicapzi');
-//$konekcija=mysqli_connect('localhost','fpmoz172021','csdigital2021','fpmoz172021');
+//$konekcija=mysqli_connect('localhost','root','','stranicapzi');
+$konekcija=mysqli_connect('localhost','fpmoz172021','csdigital2021','fpmoz172021');
 
 if(!$konekcija){
     die ("Conection failed!" . mysqli_connect_error());
